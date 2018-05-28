1beb073e37766681a909781a64b739499e869656
klaro115 <klaro115@hotmail.com>
2018-05-28 13:34:43 +0200

    Added level and wave classes + test prefabs.

---
+++ Assets/3_Scripts/Level/Level.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Game
+{
+	[CreateAssetMenu(menuName="Create Level", fileName="new Level")]
+	[System.Serializable]
+	public class Level : ScriptableObject
+	{
+		#region Fields
+
+		public Wave[] waves;				// Waves in this level in chronological order of appearance.
+		public float waveInterval = 1.0f;	// Time interval between the end of one wave and the start of another.
+		public int completionBonus = 20;	// Bonus points for completing this level.
+
+		#endregion
+	}
+}
---
+++ Assets/3_Scripts/Level/Wave.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Game
+{
+	[CreateAssetMenu(menuName="Create Wave prefab", fileName="new Wave")]
+	[System.Serializable]
+	public class Wave : ScriptableObject
+	{
+		#region Fields
+
+		public Enemy[] enemies;		// Enemy prefabs of the wave, in chronological order.
+		public float spawnInterval;	// Time delay between spawning enemy instances in scene.
+
+		#endregion
+	}
+}
