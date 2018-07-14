3f3c0e187866824c7ab1e7118f7c13e51c99b66e
DennisRitter <dennis.r.ritter@gmail.com>
2018-07-14 21:37:27 +0200

    Added second level

--- Assets/3_Scripts/Statemachine/StatemachineStates/StatemachineStateIngame.cs
+++ Assets/3_Scripts/Statemachine/StatemachineStates/StatemachineStateIngame.cs
@@ -24,7 +24,7 @@ namespace Game
 
 		private static readonly string playerPrefabName = "Player";
 		private static readonly string uiIngamePrefabName = "UiIngame";
-		private static readonly string firstLevelAssetName = "Level 1";
+		private static readonly string firstLevelAssetName = "Level 2";
 
 		#endregion
 		#region Properties
