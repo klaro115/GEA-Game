c0dd55f27481f0fcd3c55b66c913a3b00405f86c
klaro115 <klaro115@hotmail.com>
2018-07-15 19:15:00 +0200

    Game now starting with level 1.

--- Assets/3_Scripts/Statemachine/StatemachineStates/StatemachineStateIngame.cs
+++ Assets/3_Scripts/Statemachine/StatemachineStates/StatemachineStateIngame.cs
@@ -27,7 +27,7 @@ namespace Game
 
     private static readonly string playerPrefabName = "Player";
 		private static readonly string uiIngamePrefabName = "UiIngame";
-		private static readonly string firstLevelAssetName = "Level 2";
+		private static readonly string firstLevelAssetName = "Level 1";
 
 		#endregion
 		#region Properties
